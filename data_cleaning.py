4c3baed17e14f5bfb7ac04fa814de6202a61498a
prg48 <32218486+prg48@users.noreply.github.com>
2023-06-12 14:48:46 +0100

    Initialise the three project classes

---
+++ data_cleaning.py
@@ -0,0 +1,3 @@
+class DataCleaning:
+    # this script will contain a class DataCleaning with methods to clean data from each of the data sources.
+    pass
---
+++ database_utils.py
@@ -0,0 +1,3 @@
+class DatabaseConnector:
+    # This class will be used to connect with and upload data to the database.
+    pass
