465b98cd429150fa51ef8e4f8a6e225058ce66af
prg48 <32218486+prg48@users.noreply.github.com>
2023-06-12 15:56:03 +0100

    create read_db_creds method in database_utils

--- database_utils.py
+++ database_utils.py
@@ -1,3 +1,29 @@
+#%%
+import yaml
+
 class DatabaseConnector:
     # This class will be used to connect with and upload data to the database.
-    pass
+
+    @classmethod
+    def read_db_creds(cls, yaml_filepath: str) -> dict:
+        """
+        This method reads the credentials in a yaml file and return a dictionary of the credentials.
+        Args:
+            yaml_filepath (str): file path to the yaml file
+        
+        Returns:
+            dict: key value pair from the yaml file
+        """
+        try:
+            with open(yaml_filepath, "r") as file:
+                data = yaml.safe_load(file)
+            return data
+        except yaml.YAMLError as e:
+            print(f"File configuration error: {e}")
+
+
+
+#%%
+yaml_config = DatabaseConnector.read_db_creds("db_creds.yaml")
+type(yaml_config)
+# %%
